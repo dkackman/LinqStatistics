719ee3530a22536774a8086ee11232602df7dbe4
dkackman <dkackman@gmail.com>
2014-08-07 22:36:22 -0500

    add histogram unit test

---
+++ LinqStatisticsTests/HistogramTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using LinqStatistics;
+
+namespace LinqStatisticsTests
+{
+    [TestClass]
+    public class HistogramTests
+    {
+        [TestMethod]
+        public void SimpleHistogram()
+        {
+            var list = new List<int>()
+            {
+                0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 7
+            };
+
+            var histogram = list.Histogram().ToList();
+
+            Assert.AreEqual(histogram.Count(), list.Distinct().Count());
+
+            Assert.AreEqual(histogram[0].Value, 0);
+            Assert.AreEqual(histogram[0].Count, 1);
+
+            Assert.AreEqual(histogram[1].Value, 1);
+            Assert.AreEqual(histogram[1].Count, 2);
+
+            Assert.AreEqual(histogram[2].Value, 2);
+            Assert.AreEqual(histogram[2].Count, 3);
+
+            Assert.AreEqual(histogram[3].Value, 3);
+            Assert.AreEqual(histogram[3].Count, 4);
+
+            Assert.AreEqual(histogram[4].Value, 4);
+            Assert.AreEqual(histogram[4].Count, 3);
+
+            Assert.AreEqual(histogram[5].Value, 5);
+            Assert.AreEqual(histogram[5].Count, 3);
+
+            Assert.AreEqual(histogram[6].Value, 6);
+            Assert.AreEqual(histogram[6].Count, 2);
+
+            Assert.AreEqual(histogram[7].Value, 7);
+            Assert.AreEqual(histogram[7].Count, 1);
+        }
+    }
+}
