4b1bc17bfd91015abd45a82bb64571e1471bb719
Ace Olszowka <aolszowka@gmail.com>
2014-11-02 14:31:33 -0700

    Added some unit tests to MedianTests around longs, including some possible edge cases.

--- LinqStatisticsTests/TestData.cs
+++ LinqStatisticsTests/TestData.cs
@@ -30,6 +30,21 @@ namespace LinqStatistics.UnitTests
         public static IEnumerable<float> GetFloats()
         {
             return new float[] { 2.1f, 3.5f, 4.6f, 6.9f };
-        }        
+        }
+
+        public static IEnumerable<long> GetLongsEven()
+        {
+            return new long[] { 4298189245, 6256318983, 6220208083, 5117161634, 4145865333, 8206231274, 5791942653, 7493199652, 7100498765, 8849245969, 6576317651, 7516910657, 4110237458, 9528829097, 9193648296, 9535539842, 9223372036854775807, 9223372036854775806 };
+        }
+
+        public static IEnumerable<long> GetMaxLongsEven()
+        {
+            return new long[] { long.MaxValue, long.MaxValue, long.MaxValue, long.MaxValue };
+        }
+
+        public static IEnumerable<long> GetMinimumLongsEven()
+        {
+            return new long[] { long.MinValue, long.MinValue, long.MinValue, long.MinValue };
+        }
     }
 }
