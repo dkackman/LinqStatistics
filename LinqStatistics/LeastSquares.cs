e7bdd690d0ac3661ad49432a0adc8b33695e90d2
Don Kackman <dkackman@gmail.com>
2016-01-11 19:04:17 -0600

    commenting

--- LinqStatistics/LeastSquares.cs
+++ LinqStatistics/LeastSquares.cs
@@ -48,6 +48,8 @@ namespace LinqStatistics
         /// <returns>X value (x = (y - b) / m)</returns>
         public double SolveForX(double y)
         {
+            if (M == 0.0) throw new InvalidOperationException("Cannot solve for X when the equation slope (M) is zero");
+
             return (y - B) / M;
         }
 
