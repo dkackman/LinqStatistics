f0b781f7a90dab387798728bf936e07bf76b544b
Alexey Sednev <alexey@sednev.com>
2016-04-18 22:57:19 -0700

    Add NaN test

---
+++ LinqStatisticsTests/NaN/RootMeanSquareNaNTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using LinqStatistics.NaN;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqStatistics.UnitTests.NaN
+{
+    [TestClass]
+    public class RootMeanSquareNaNTests
+    {
+        [TestMethod]
+        public void RootMeanSquareDoubleNaN()
+        {
+            IEnumerable<double> source = new double[] { };
+
+            double result = source.RootMeanSquareNaN();
+
+            Assert.AreEqual(double.NaN, result);
+        }
+
+        [TestMethod]
+        public void RootMeanSquareNullableDoubleNaN()
+        {
+            IEnumerable<double?> source = new double?[] { };
+
+            double? result = source.RootMeanSquareNaN();
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void RootMeanSquareFloatNaN()
+        {
+            IEnumerable<float> source = new float[] { };
+
+            float result = source.RootMeanSquareNaN();
+
+            Assert.AreEqual(float.NaN, result);
+        }
+
+        [TestMethod]
+        public void RootMeanSquareNullableFloatNaN()
+        {
+            IEnumerable<float?> source = new float?[] { };
+
+            float? result = source.RootMeanSquareNaN();
+
+            Assert.IsNull(result);
+        }
+
+    }
+}
