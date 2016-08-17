1711adc0cd2dc65df121e476f50404f410d462db
dkackman <dkackman@gmail.com>
2016-08-16 20:22:43 -0500

    add xunit test for dotnet core testing on linux

---
+++ test/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using LinqStatistics.NaN;
+
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            var list = new List<int>();
+
+            double mean = list.AverageNaN();
+
+            Assert.True(double.IsNaN(mean));
+        }
+    }
+}
