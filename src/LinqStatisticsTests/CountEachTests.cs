2387489f30680d628ad8cc3b7c25e72287014647
Don Kackman <dkackman@gmail.com>
2020-01-04 16:50:06 -0600

    unit tests for CountEach

--- src/LinqStatisticsTests/CountEachTests.cs
+++ src/LinqStatisticsTests/CountEachTests.cs
@@ -20,7 +20,7 @@ namespace LinqStatistics.UnitTests
             };
 
             var eaches = list.CountEach();
-                       
+
             Assert.AreEqual(4, eaches.Count());
 
             Assert.AreEqual(1, eaches.First().RepresentativeValue);
@@ -34,6 +34,70 @@ namespace LinqStatistics.UnitTests
 
             Assert.AreEqual(4, eaches.Skip(3).First().RepresentativeValue);
             Assert.AreEqual(2, eaches.Skip(3).First().Count);
+        }
+
+        struct Tmp : IComparable
+        {
+            public int x;
+            public int y;
+
+            public int CompareTo(object obj)
+            {
+                if (obj is Tmp other)
+                {
+                    return (x + y) - (other.x + other.y);
+                }
+
+                return -1;
+            }
+        }
+
+        [TestMethod]
+        public void CountEachSelector()
+        {
+            var list = new List<Tmp>()
+             {
+                new Tmp { x = 1, y = 1 },
+                new Tmp { x = 2, y = 1 }
+            };
+
+            var eaches = list.CountEach(item => item.y);
+
+            Assert.AreEqual(1, eaches.Count());
+
+            Assert.AreEqual(1, eaches.First().RepresentativeValue);
+            Assert.AreEqual(2, eaches.First().Count);
+        }
+
+        class TmpComparer : IEqualityComparer<Tmp>
+        {
+            public bool Equals(Tmp a, Tmp b)
+            {
+                return a.x == b.x && a.y == b.y;
+            }
+
+            public int GetHashCode(Tmp obj)
+            {
+                return 13 ^ obj.x.GetHashCode() ^ obj.y.GetHashCode();
+            }
+        }
+
+        [TestMethod]
+        public void CountEachComprarer()
+        {
+
+            var list = new List<Tmp>()
+             {
+                new Tmp { x = 1, y = 1 },
+                new Tmp { x = 2, y = 1 }
+            };
+
+            var eaches = list.CountEach(new TmpComparer());
+
+            Assert.AreEqual(2, eaches.Count());
+
+            Assert.IsTrue(eaches.First().RepresentativeValue.x == 1 && eaches.First().RepresentativeValue.y == 1);
+            Assert.IsTrue(eaches.Last().RepresentativeValue.x == 2 && eaches.Last().RepresentativeValue.y == 1);
         }
     }
 }
